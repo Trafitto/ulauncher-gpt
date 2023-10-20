b3d78b99818581d9c656bd353f6763e3870c5b02
Trafitto <borchimarco@gmail.com>
2023-10-21 00:22:28 +0200

    add personality

--- gpt.py
+++ gpt.py
@@ -1,26 +1,30 @@
 import requests
 import json
 
-OPEN_API_CHAT_URL =  'https://api.openai.com/v1/chat/completions'
+OPEN_API_CHAT_URL = 'https://api.openai.com/v1/chat/completions'
+
 
 class GPT:
     def __init__(self, ulauncer_preferences):
+        self.messages = []
         self.headers = {
             "Content-Type": "application/json",
             "Authorization": f"Bearer {ulauncer_preferences['OPENAI_API_KEY']}",
         }
-        self.messages = [] # TODO: Add personality
+        if ulauncer_preferences['PERONALITY']:
+            self.messages.append(
+                {"role": "system", "content": ulauncer_preferences['PERONALITY']})
         self.data = {
-             "model": ulauncer_preferences['MODEL'],
+            "model": ulauncer_preferences['MODEL'],
             "temperature": int(ulauncer_preferences['TEMPERATURE'])
         }
-        
+
     def ask(self, message: str) -> str:
         self.messages.append({"role": "user", "content": message})
         self.data['messages'] = self.messages
         response = requests.post(
             OPEN_API_CHAT_URL, headers=self.headers, data=json.dumps(self.data))
-
+        print(response)
         if response.status_code == 200:
             return response.json()["choices"][0]["message"]["content"]
         else:
